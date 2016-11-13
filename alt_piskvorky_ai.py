7bc5d9f7ddefa5c0f39cf2d1943adc1deee6c047
taryodor <matej.lachman@email.cz>
2016-11-13 11:21:43 +0100

    Add files via upload
    Definitivni verze alt_ai piskvorek.
    Posledni upravy jsou ryze funkcniho charakteru. Provedl jsem takove zmeny, aby se nove podminky lehce pridavaly/ubiraly.
    Vim, ze na nej uz nejspis nesahnu, ale chtel jsem to mit v tak dobre forme, co v soucasnosti dokazu :-)

--- alt_piskvorky_ai.py
+++ alt_piskvorky_ai.py
@@ -1,13 +1,41 @@
 import random
 
+def overeni_pole_4_podminky(i, mensi_nez_i, pole, omezeni_pole , i1, i2, i3, i4, p1, p2, p3, p4):
+    #funkce nahrazujici if se ctyrma podminkama, rozsah i>mensi_nez_i,len-omezeni_pole,
+    lokalni_seznam = []
+    if i>mensi_nez_i and i<len(pole)+omezeni_pole:
+        if pole[i1]==p1 and pole[i2]==p2 and pole[i3]==p3 and pole[i4]==p4:
+            lokalni_seznam.append(i)
+            x=random.choice(lokalni_seznam)
+            return x
+
+def overeni_pole_3_podminky(i, mensi_nez_i, pole, omezeni_pole , i1, i2, i3, p1, p2, p3):
+    #funkce nahrazujici if se trema podminkama, rozsah i>mensi_nez_i,len-omezeni_pole
+    lokalni_seznam = []
+    if i>mensi_nez_i and i<len(pole)+omezeni_pole:
+        if pole[i1]==p1 and pole[i2]==p2 and pole[i3]==p3:
+            lokalni_seznam.append(i)
+            x=random.choice(lokalni_seznam)
+            return x
+
+def overeni_pole_5_podminek_1_not(i, mensi_nez_i, pole, omezeni_pole , i1, i2, i3, i4, i5, p1, p2, p3, p4, p5):
+    #funkce nahrazujici if se trema podminkama, rozsah i>mensi_nez_i,len-omezeni_pole
+    lokalni_seznam = []
+    if i>mensi_nez_i and i<len(pole)+omezeni_pole:
+        if pole[i1]!=p1 and pole[i2]==p2 and pole[i3]==p3 and pole[i4]==p4 and pole[i5]==p5:
+            lokalni_seznam.append(i)
+            x=random.choice(lokalni_seznam)
+            return x
+
 def alt_ai(pole, ai_symbol, eny_symbol, volno_symbol):
+    #Tady si vytvorim dvourozmerny seznam pro ukladani priorit hrani, cim vyssi 1. rozmer, tim spis by to progam mel hrat
+    #Na konci algoritmu si nahodne vyberu ze seznamu s nejvyssi nalezenou prioritou
     seznam_priorit = []
     for i in range(8):
         seznam_priorit.append([])
 
     for i in range(0, len(pole)):#nejdriv testuju priority 1 a dva, tj. volne pozice resp. hratelne 3. poz od kraje
-        #jen se ujistim, ze pole bude mit tolik znaku, kolik potrebuji,
-        #seznam_priorit[0].append(i)
+        #jen se ujistim, ze priorizovane pole bude mit tolik znaku, kolik potrebuji,
         if pole[i] == volno_symbol:
             seznam_priorit[1].append(i)
         if pole[i-2] == volno_symbol and pole[i-1]==volno_symbol and i==2 and pole[i+1]==volno_symbol and pole[i+2]!=eny_symbol:
@@ -16,85 +44,43 @@ def alt_ai(pole, ai_symbol, eny_symbol, volno_symbol):
             if pole[i-2]!=eny_symbol and pole[i-1]==volno_symbol and i==len(pole)-3 and pole[i+1]==volno_symbol and pole[i+1]==volno_symbol:
                 seznam_priorit[2].append(i)
 
-        #ted jdu na prior. 3 tj. prilozeni k me pozici, vyroba 1/2diry
-        if i>1 and i<len(pole)-1:#samostatna pozice s mojim symbolem vlevo
-            if pole[i]==volno_symbol and pole[i+1]==volno_symbol and pole[i-2]==volno_symbol and pole[i-1]==ai_symbol:
-                seznam_priorit[3].append(i)
-        if i>0 and i<len(pole)-2:#samostatna pozice s mojim symbolem vpravo
-            if pole[i-1]==volno_symbol and pole[i]==volno_symbol and pole[i+1]==ai_symbol and pole[i+2]==volno_symbol:
-                seznam_priorit[3].append(i)
-        #vpravo od pozice se da hrat
-        if i>0 and i<len(pole)-1:
-            if pole[i-1]==ai_symbol and pole[i]==volno_symbol and pole[i+1]==volno_symbol:
-                seznam_priorit[3].append(i)
-        #vlevo od pozice se da hrat
-        if i>1 and i<len(pole)-1:
-            if pole[i-1]==volno_symbol and pole[i]==volno_symbol and pole[i+1]==ai_symbol:
-                seznam_priorit[3].append(i)
-        #ted jdu vyrobyt jednodiru od me pozice vpravo
-        if i>1 and i<len(pole):
-            if pole[i-2]==ai_symbol and pole[i-1]==volno_symbol and pole[i]==volno_symbol:
-                seznam_priorit[3].append(i)
-        #ted jdu vyrobyt jednodiru od me pozice vlevo
-        if i<len(pole)-2:
-            if pole[i]==volno_symbol and pole[i+1]==volno_symbol and pole[i+2]==ai_symbol:
-                seznam_priorit[3].append(i)
-        #Vyroba dvoudiry od eny pozice vlevo
-        if i>0 and i<len(pole)-3:
-            if pole[i-1]!=eny_symbol and pole[i]==volno_symbol and pole[i+1]==volno_symbol and pole[i+2]==volno_symbol and pole[i+3]==eny_symbol:
-                seznam_priorit[3].append(i)
-        #Vyroba dvoudiry od eny pozice vpravo
-        if i>3 and i<len(pole)-1:
-            if pole[i+1]!=eny_symbol and pole[i]==volno_symbol and pole[i-1]==volno_symbol and pole[i-2]==volno_symbol and pole[i-3]==eny_symbol:
-                seznam_priorit[3].append(i)
-
-        #blokace eny volne pozice vlevo
-        if i<len(pole)-2:
-            if pole[i]==volno_symbol and pole[i+1]==eny_symbol and pole[i+2]==volno_symbol:
-                seznam_priorit[4].append(i)
-        #blokace eny volne pozice vlevo
-        if i>1 and i<len(pole):
-            if pole[i-2]==volno_symbol and pole[i-1]==eny_symbol and pole[i]==volno_symbol:
-                seznam_priorit[4].append(i)
-
-        #oblozeni me volne pozice vlevo
-        if i>0 and i<len(pole)-2:
-            if pole[i-1]==volno_symbol and pole[i]==volno_symbol and pole[i+1]==ai_symbol and pole[i+2]==volno_symbol:
-                seznam_priorit[5].append(i)
-        #oblozeni me volne pozice vpravo
-        if i>1 and i<len(pole)-1:
-            if pole[i-2]==volno_symbol and pole[i-1]==ai_symbol and pole[i]==volno_symbol and pole[i+1]==volno_symbol:
-                seznam_priorit[5].append(i)
-
-        #blokace eny dvoupozice vlevo
-        if i<len(pole)-2:
-            if pole[i]==volno_symbol and pole[i+1]==eny_symbol and pole[i+2]==eny_symbol:
-                seznam_priorit[6].append(i)
-        #blokace eny dvoupozice vpravo
-        if i>1:
-            if pole[i-2]==eny_symbol and pole[i-1]==eny_symbol and pole[i]==volno_symbol:
-                seznam_priorit[6].append(i)
+        #Jdu na prior. 3 tj. prilozeni k me pozici, vyroba 1/2diry
+        seznam_priorit[3].append(overeni_pole_4_podminky(i, 1, pole, -1, i, i+1, i-2, i-1, volno_symbol, volno_symbol, volno_symbol, ai_symbol))
+        seznam_priorit[3].append(overeni_pole_4_podminky(i, 0, pole, -2, i-1, i, i+1, i+2, volno_symbol, volno_symbol, ai_symbol, volno_symbol))
+        #vpravo+vlevo od pozice se da hrat
+        seznam_priorit[3].append(overeni_pole_3_podminky(i, 0, pole, -1, i-1, i, i+1, ai_symbol, volno_symbol, volno_symbol))
+        seznam_priorit[3].append(overeni_pole_3_podminky(i, 1, pole, -1, i-1, i, i+1, volno_symbol, volno_symbol, ai_symbol))
+        #vyrobyt jednodiru od me pozice vpravo+vlevo
+        seznam_priorit[3].append(overeni_pole_3_podminky(i, 1, pole, 0, i-2, i-1, i, ai_symbol, volno_symbol, volno_symbol))
+        seznam_priorit[3].append(overeni_pole_3_podminky(i, -1, pole, -2, i, i+1, i+2, volno_symbol, volno_symbol, ai_symbol))
+        #Vyroba dvoudiry od eny pozice vlevo+vpravo
+        seznam_priorit[3].append(overeni_pole_5_podminek_1_not(i, 0, pole, -3, i-1, i, i+1, i+2, i+3, eny_symbol, volno_symbol, volno_symbol, volno_symbol, eny_symbol))
+        seznam_priorit[3].append(overeni_pole_5_podminek_1_not(i, 3, pole, -1, i+1, i, i-1, i-2, i-3, eny_symbol, volno_symbol, volno_symbol, volno_symbol, eny_symbol))
+        #blokace eny volne pozice vlevo+vpravo
+        seznam_priorit[4].append(overeni_pole_3_podminky(i, -1, pole, -2, i, i+1, i+2, volno_symbol, eny_symbol, volno_symbol))
+        seznam_priorit[4].append(overeni_pole_3_podminky(i, 1, pole, 0, i-2, i-1, i, volno_symbol, eny_symbol, volno_symbol))
+        #oblozeni me volne pozice vlevo+vpravo
+        seznam_priorit[5].append(overeni_pole_4_podminky(i, 0, pole, -2, i-1, i, i+1, i+2, volno_symbol, volno_symbol, ai_symbol, volno_symbol))
+        seznam_priorit[5].append(overeni_pole_4_podminky(i, 1, pole, -1, i-2, i-1, i, i+1, volno_symbol, ai_symbol, volno_symbol, volno_symbol))
+        #blokace eny dvoupozice vlevo+vpravo
+        seznam_priorit[6].append(overeni_pole_3_podminky(i, -1, pole, -2, i, i+1, i+2, volno_symbol, eny_symbol, eny_symbol))
+        seznam_priorit[6].append(overeni_pole_3_podminky(i, 1, pole, 1, i-2, i-1, i, eny_symbol, eny_symbol, volno_symbol))
         #blokace eny mezy kozy
-        if i>0 and i<len(pole)-1:
-            if pole[i-1]==eny_symbol and pole[i]==volno_symbol and pole[i+1]==eny_symbol:
-                seznam_priorit[6].append(i)
+        seznam_priorit[6].append(overeni_pole_3_podminky(i, 0, pole, -1, i-1, i, i+1, eny_symbol, volno_symbol, eny_symbol))
+        #ai hrani mezy kozy
+        seznam_priorit[7].append(overeni_pole_3_podminky(i, 0, pole, -1, i-1, i, i+1, ai_symbol, volno_symbol, ai_symbol))
+        #dokonceni me dvoupozice vlevo+vpravo
+        seznam_priorit[7].append(overeni_pole_3_podminky(i, -1, pole, -2, i, i+1, i+2, volno_symbol, ai_symbol, ai_symbol))
+        seznam_priorit[7].append(overeni_pole_3_podminky(i, 1, pole, +1, i-2, i-1, i, ai_symbol, ai_symbol, volno_symbol))
 
-        #Moje hrani mezy kozy
-        if i>0 and i<len(pole)-1:
-            if pole[i-1]==ai_symbol and pole[i]==volno_symbol and pole[i+1]==ai_symbol:
-                seznam_priorit[7].append(i)
-        #dokonceni me dvoupozice vlevo
-        if i<len(pole)-2:
-            if pole[i]==volno_symbol and pole[i+1]==ai_symbol and pole[i+2]==ai_symbol:
-                seznam_priorit[7].append(i)
-        #dokonceni me dvoupozice vpravo
-        if i>1:
-            if pole[i-2]==ai_symbol and pole[i-1]==ai_symbol and pole[i]==volno_symbol:
-                seznam_priorit[7].append(i)
+    #Odstraneni None pozic zpusobenych nepodminenym volanim overovacich funkci
+    for m in range(len(seznam_priorit)):
+        while  None in seznam_priorit[m]:
+            seznam_priorit[m].remove(None)
 
-    #Tak a na zaver vyberu pozice s nejvetsi prioritou a hraju tam
+    #Na zaver vyberu pozice s nejvetsi prioritou a hraju tam
     for k in range(len(seznam_priorit)-1,-1,-1):
         if len(seznam_priorit[k]) > 0:
             return random.choice(seznam_priorit[k])
 
-#print(alt_piskvorky_ai("--oo----x-xx--o-o--", "o", "x", "-"))
+#print(alt_ai("-o-o-o-oxx----x--", "o", "x", "-"))
